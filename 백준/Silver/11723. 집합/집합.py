38c6a6565028ba32e1ad837788a0fdb4b4361ca6
yeonu <104813592+nuyeo@users.noreply.github.com>
2024-09-02 15:45:37 +0900

    [Silver V] Title: 집합, Time: 4708 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+s = set()
+
+
+for _ in range(n):
+    string = input().rstrip().split()
+    cmd = string[0]
+    k = 0
+    if len(string) > 1:
+        k = int(string[1])
+
+    if cmd == "add":
+        s.add(k)
+    elif cmd == "remove":
+        s.discard(k)
+    elif cmd == "check":
+        if k in s:
+            print(1)
+        else:
+            print(0)
+    elif cmd == "toggle":
+        if k in s:
+            s.discard(k)
+        else:
+            s.add(k)
+    elif cmd == "all":
+        s = set([i for i in range(1, 21)])
+    elif cmd == "empty":
+        s = set()
