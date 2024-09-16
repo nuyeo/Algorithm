972e38ccffb557b88351e6180a390926c49da755
yeonu <104813592+nuyeo@users.noreply.github.com>
2024-09-16 15:25:40 +0900

    [Silver V] Title: 영화감독 숌, Time: 604 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+num = 0
+cnt = 0
+
+while True:
+    if '666' in str(num):
+        cnt += 1
+        
+    if cnt == n:
+        break
+        
+    num += 1
+    
+print(num)
