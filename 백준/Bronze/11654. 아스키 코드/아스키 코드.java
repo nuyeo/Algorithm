1633207aa8dad2085a6da0f6744a9a401107154a
yeonu <bluelayzxx@gmail.com>
2026-02-03 10:21:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 180 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.print((int) s.charAt(0));
+		
+		sc.close();
+	}
+}
