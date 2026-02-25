731a456abc84c97b83d81fc1c5e137df5d4ed22e
yeonu <bluelayzxx@gmail.com>
2026-02-25 10:24:50 +0900

    [Silver IV] Title: 수 찾기, Time: 428 ms, Memory: 52108 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,25 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int n = Integer.parseInt(br.readLine());
-        HashSet<Integer> set = new HashSet<>();
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
-        }
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        
-        for (int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            sb.append(set.contains(target) ? 1 : 0).append("\n");
-        }
-        System.out.print(sb);
-    }
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(nums);
+		
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < m; i++) {
+			int target = Integer.parseInt(st.nextToken());
+			
+			if (Arrays.binarySearch(nums, target) >= 0) {
+				sb.append(1).append("\n");
+			} else {
+				sb.append(0).append("\n");
+			}
+		}
+		
+		System.out.print(sb);
+	}
+
 }
